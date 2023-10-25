fa220ee76377ddd20d0ca6785e82007f31deb376
airon37 <airon3737@gmail.com>
2023-10-25 10:32:45 +0200

    finalizao

--- models/models.py
+++ models/models.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class tareas(models.Model):
-#     _name = 'tareas.tareas'
-#     _description = 'tareas.tareas'
+class tareas(models.Model):
+    _name = 'tareas.tareas'
+    _description = 'tareas.tareas'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    nombre = fields.Char(required=True, help="Introducir nombre")
+    descripcion = fields.Text()
+    horas = fields.Integer()
+    fecha_creacion = fields.Date()
+    fecha_comienzo = fields.Datetime()
+    fecha_fin = fields.Datetime()
+    pausada = fields.Boolean()
+    sprint = fields.Many2one("tareas.sprint")
+    tecnologias = fields.Many2many("tareas.tecnologias")
+
+class sprint(models.Model):
+    _name = 'tareas.sprint'
+    _description = 'tareas.sprint'
+
+    nombre = fields.Char()
+    descripcion = fields.Text()
+    fecha_creacion = fields.Datetime()
+    fecha_fin = fields.Datetime()
+    tarea = fields.One2many("tareas.tareas", "sprint")
+
+class tecnologias(models.Model):
+    _name = 'tareas.tecnologias'
+    _desription = 'tareas.tecnologias'
+
+    nombre = fields.Char()
+    imagen = fields.Image(maxwidth=200, maxheight=200)
